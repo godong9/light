bd163c8ea9df6e5ee05a85b34980c7d0b81ecb4f
Go Dong Hyun <godong9@gmail.com>
2013-07-25 10:45:52 +0900

    130725_v1
    app

---
+++ light_app/src/com/example/light/RivalFrag.java
@@ -0,0 +1,16 @@
+package com.example.light;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RivalFrag extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, 
+		ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.rival_frag, container, false);
+	}
+			      
+}
