49b7c65267cbd9f46d04af5b0e3debe9b18e6124
Go Dong Hyun <godong9@gmail.com>
2013-08-19 15:11:20 +0900

    130819_v4_시간계산 구현완료

--- light_app/src/app/light/CommonFragment.java
+++ light_app/src/app/light/CommonFragment.java
@@ -1,28 +1,11 @@
 package app.light;
 
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
-
+import java.sql.Timestamp;
 import android.app.Fragment;
-import android.util.Log;
-import android.webkit.CookieManager;
 
 public class CommonFragment extends Fragment {
 	
-	
+	public Timestamp getTimestamp(String str){
+		return Timestamp.valueOf(str);
+	}
 }
--- light_app/src/app/light/RivalFrag.java
+++ light_app/src/app/light/RivalFrag.java
@@ -1,18 +1,27 @@
 package app.light;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RivalFrag extends CommonFragment {
@@ -127,9 +136,47 @@ public class RivalFrag extends CommonFragment {
 					}
 				}
 				
+				
+				Calendar cal = Calendar.getInstance();
+
+				String today_string = String.format("%04d-%02d-%02d", cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH));
+				//timeToString = String.format("%02d:%02d:%02d", cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND));
+				
+				//Calendar start_cal = Calendar.getInstance();
+				//Calendar end_cal = Calendar.getInstance();
+				
+				String start_day_str = group_info.getString("start_date");
+				String end_day_str = group_info.getString("end_date");
+			
+				String start_day = start_day_str.substring(0, start_day_str.indexOf("T"));
+				String end_day = end_day_str.substring(0, end_day_str.indexOf("T"));
+				
+				
+				
+				SimpleDateFormat tf = new SimpleDateFormat("yyyy-MM-dd");
+				Date start_day_date = tf.parse(start_day);
+				Date end_day_date = tf.parse(end_day);
+				
+				long diff = end_day_date.getTime() - start_day_date.getTime();
+				long diffDays = diff / (24*60*60*1000);
+				
+				
+				//start_cal.set(start_day_date.getYear(), start_day_date.getMonth(), start_day_date.getDay());
+				//end_cal.set(end_day_date.getYear(), end_day_date.getMonth(), end_day_date.getDay());
+				
+				//long rawResult = (end_cal.getTimeInMillis() - start_cal.getTimeInMillis()) / 1000;
+				//long result = rawResult/(60*60*24);
+				
+				
+				
+				System.out.println("날짜차이 : "+diffDays);
 				System.out.println(rival_info.getJSONObject(0).getString("email"));
 				System.out.println(rival_info.getJSONObject(1).getString("email"));
 				System.out.println(rival_info.getJSONObject(2).getString("email"));
+				
+				TextView tvGroupCountDay = (TextView)((Activity)context).findViewById(R.id.rival_count_day);
+				TextView tvGroupRemainDay = (TextView)((Activity)context).findViewById(R.id.rival_remain_day);    
+				TextView tvGroupGoal = (TextView)((Activity)context).findViewById(R.id.rival_group_goal);
 				
 				
 			//	System.out.println("GROUP->"+json_group_info);
