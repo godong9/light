68142ae1b7e3464e405ea337bc91ccbb9ca5a727
godong9 <godong9@gmail.com>
2013-08-24 02:56:37 +0900

    130824_server_upload

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ exports.index = function(req, res){
 };
 
 exports.upload = function(req, res){
-	console.log('-> upload was called\n\n');
-	console.log('-> ' +  util.inspect(req.files.uploadedfile));        
+	console.log('->> upload was called!');
+//	console.log('-> ' +  util.inspect(req.files.uploadedfile));        
 	var images = [];
 
-	req.addListener('data', function(chunk) {
-		console.log('-> data ' + chunk);
-	});
+//	req.addListener('data', function(chunk) {
+//		console.log('-> data ' + chunk);
+//	});
 
 	var image = req.files.uploadedfile;
 	var kb = image.size / 1024 | 0;
@@ -20,7 +20,7 @@ exports.upload = function(req, res){
 	images.push({name: image.name, size: kb});
 	renameImg(image);
  
-	console.log('->> render');
+//	console.log('->> render');
 	res.send( { title: 'success', msg: 'Image Upload Done' });
 };
 
@@ -29,7 +29,7 @@ function renameImg(image){
 	
 	var file_name_array = image.name.split('/');
     var target_path = './public/upload/' + file_name_array[3];
-    console.log('->> tmp_path: ' + tmp_path );
+//  console.log('->> tmp_path: ' + tmp_path );
     console.log('->> target_path: ' + target_path );
             
     fs.rename(tmp_path, target_path, function(err){
