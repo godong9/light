a3fc6d264a939d8681c60e42dc8cb35042cfcd1b
godong9 <godong9@gmail.com>
2013-09-07 19:06:08 +0900

    130907_server_2

--- sql/timeline.js
+++ sql/timeline.js
@@ -73,8 +73,41 @@ exports.dao_get_group_reg_id = function(evt, mysql_conn, params){
 // params['content']
 // params['calorie']
 exports.dao_set_timeline = function(evt, mysql_conn, params){
-	
-	if(params['type'] == '7'){	//체중 기록일 때
+	if(params['type'] == '4'){	//식사 기록일 때
+		//console.log("food Update");
+		var pre_sql = "SELECT ";
+		pre_sql += "`food_calorie` "; 
+		pre_sql += "FROM `user` "; 
+		pre_sql += "WHERE `email` = '"+params['email']+"' ";
+		var pre_query = mysql_conn.query(pre_sql, params, function(err, rows, fields) {
+			var tmp_food_calorie = Number(rows[0].food_calorie) + Number(params['calorie']);
+			//console.log("food => "+tmp_food_calorie);				
+			var u_sql = "UPDATE `user` ";
+			u_sql += "SET `food_calorie` = '"+tmp_food_calorie+"' "; 
+			u_sql += "WHERE `email` = '"+params['email']+"' ";
+			var u_query = mysql_conn.query(u_sql, params, function(err, rows, fields) {
+				//console.log("update food_calorie");	
+			});
+		}); 
+	}
+	else if(params['type'] == '5'){	//운동 기록일 때
+		//console.log("exercise Update");
+		var pre_sql = "SELECT ";
+		pre_sql += "`exercise_calorie` "; 
+		pre_sql += "FROM `user` "; 
+		pre_sql += "WHERE `email` = '"+params['email']+"' ";
+		var pre_query = mysql_conn.query(pre_sql, params, function(err, rows, fields) {
+			var tmp_exercise_calorie = Number(rows[0].exercise_calorie) + Number(params['calorie']);
+			//console.log("exercise => "+tmp_exericse_calorie);				
+			var u_sql = "UPDATE `user` ";
+			u_sql += "SET `exercise_calorie` = '"+tmp_exercise_calorie+"' "; 
+			u_sql += "WHERE `email` = '"+params['email']+"' ";
+			var u_query = mysql_conn.query(u_sql, params, function(err, rows, fields) {
+				//console.log("update exercise_calorie");	
+			});
+		}); 
+	}
+	else if(params['type'] == '7'){	//체중 기록일 때
 		console.log("weight Update");
 		var pre_sql = "UPDATE `user` ";
 		pre_sql += "SET `weight` = '"+params['content']+"' "; 
@@ -83,6 +116,7 @@ exports.dao_set_timeline = function(evt, mysql_conn, params){
 			console.log("weight Update");	
 		});
 	}
+
 	var sql = "INSERT INTO `timeline` ";
 	sql += "SET `email` = '"+params['email']+"', ";
 	sql += "`group_id` = '"+params['group_id']+"', ";
@@ -91,6 +125,7 @@ exports.dao_set_timeline = function(evt, mysql_conn, params){
 	sql += "`content` = '"+params['content']+"', ";
 	sql += "`calorie` = '"+params['calorie']+"'";
 	var query = mysql_conn.query(sql, params, function(err, rows, fields) {
+		console.log("insert Complete");
 		evt.emit('set_timeline', err, rows);
 	});
 	return sql;
