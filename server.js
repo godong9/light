643d9f08d9ad1c86be0d4a7d81e62fcc486960db
godong9 <godong9@gmail.com>
2013-09-16 23:20:08 +0900

    130916_server

--- server.js
+++ server.js
@@ -18,6 +18,25 @@ var d0 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 0}, function(){
 	});
 });
 
+var m0 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 0}, function(){
+	var email = 'allightdiet@gmail.com';
+	var group_id = 0;
+	var view_type = 2;
+	var pre_content = '측정';
+	var content = '몸무게 측정해서 기록하기!';
+
+	var sql = "INSERT INTO `timeline` ";
+	sql += "SET `email` = '"+email+"', ";
+	sql += "`group_id` = '"+group_id+"', ";
+	sql += "`view_type` = '"+view_type+"', ";
+	sql += "`pre_content` = '"+pre_content+"', ";
+	sql += "`content` = '"+content+"' ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		console.log("Sunday Mission Insert!");
+	});
+});
+
 var d1 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 1}, function(){
     var email = 'allightdiet@gmail.com';
 	var group_id = 0;
@@ -32,6 +51,25 @@ var d1 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 1}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 1 Insert!");
+	});
+});
+
+var m1 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 1}, function(){
+	var email = 'allightdiet@gmail.com';
+	var group_id = 0;
+	var view_type = 2;
+	var pre_content = '식단';
+	var content = '평소 양의 2/3만큼만 먹기!';
+
+	var sql = "INSERT INTO `timeline` ";
+	sql += "SET `email` = '"+email+"', ";
+	sql += "`group_id` = '"+group_id+"', ";
+	sql += "`view_type` = '"+view_type+"', ";
+	sql += "`pre_content` = '"+pre_content+"', ";
+	sql += "`content` = '"+content+"' ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		console.log("Monday Mission Insert!");
 	});
 });
 
@@ -52,6 +90,25 @@ var d2 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 2}, function(){
 	});
 });
 
+var m2 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 2}, function(){
+	var email = 'allightdiet@gmail.com';
+	var group_id = 0;
+	var view_type = 2;
+	var pre_content = '식단';
+	var content = '인스턴트 음식 먹지 않기!';
+
+	var sql = "INSERT INTO `timeline` ";
+	sql += "SET `email` = '"+email+"', ";
+	sql += "`group_id` = '"+group_id+"', ";
+	sql += "`view_type` = '"+view_type+"', ";
+	sql += "`pre_content` = '"+pre_content+"', ";
+	sql += "`content` = '"+content+"' ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		console.log("Tuesday Mission Insert!");
+	});
+});
+
 var d3 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 3}, function(){
     var email = 'allightdiet@gmail.com';
 	var group_id = 0;
@@ -66,6 +123,25 @@ var d3 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 3}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 3 Insert!");
+	});
+});
+
+var m3 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 3}, function(){
+	var email = 'allightdiet@gmail.com';
+	var group_id = 0;
+	var view_type = 2;
+	var pre_content = '운동';
+	var content = '엘리베이터 이용하지 않기!';
+
+	var sql = "INSERT INTO `timeline` ";
+	sql += "SET `email` = '"+email+"', ";
+	sql += "`group_id` = '"+group_id+"', ";
+	sql += "`view_type` = '"+view_type+"', ";
+	sql += "`pre_content` = '"+pre_content+"', ";
+	sql += "`content` = '"+content+"' ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		console.log("Wednesday Mission Insert!");
 	});
 });
 
@@ -86,6 +162,25 @@ var d4 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 4}, function(){
 	});
 });
 
+var m4 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 4}, function(){
+	var email = 'allightdiet@gmail.com';
+	var group_id = 0;
+	var view_type = 2;
+	var pre_content = '식단';
+	var content = '기름진 음식 줄이기!';
+
+	var sql = "INSERT INTO `timeline` ";
+	sql += "SET `email` = '"+email+"', ";
+	sql += "`group_id` = '"+group_id+"', ";
+	sql += "`view_type` = '"+view_type+"', ";
+	sql += "`pre_content` = '"+pre_content+"', ";
+	sql += "`content` = '"+content+"' ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		console.log("Thursday Mission Insert!");
+	});
+});
+
 var d5 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 5}, function(){
     var email = 'allightdiet@gmail.com';
 	var group_id = 0;
@@ -103,6 +198,25 @@ var d5 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 5}, function(){
 	});
 });
 
+var m5 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 5}, function(){
+	var email = 'allightdiet@gmail.com';
+	var group_id = 0;
+	var view_type = 2;
+	var pre_content = '운동';
+	var content = '가까운 거리는 걸어가기!';
+
+	var sql = "INSERT INTO `timeline` ";
+	sql += "SET `email` = '"+email+"', ";
+	sql += "`group_id` = '"+group_id+"', ";
+	sql += "`view_type` = '"+view_type+"', ";
+	sql += "`pre_content` = '"+pre_content+"', ";
+	sql += "`content` = '"+content+"' ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		console.log("Friday Mission Insert!");
+	});
+});
+
 var d6 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 6}, function(){
     var email = 'allightdiet@gmail.com';
 	var group_id = 0;
@@ -117,5 +231,24 @@ var d6 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 6}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 6 Insert!");
+	});
+});
+
+var m6 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 6}, function(){
+	var email = 'allightdiet@gmail.com';
+	var group_id = 0;
+	var view_type = 2;
+	var pre_content = '운동';
+	var content = '30분 이상 걷기!';
+
+	var sql = "INSERT INTO `timeline` ";
+	sql += "SET `email` = '"+email+"', ";
+	sql += "`group_id` = '"+group_id+"', ";
+	sql += "`view_type` = '"+view_type+"', ";
+	sql += "`pre_content` = '"+pre_content+"', ";
+	sql += "`content` = '"+content+"' ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		console.log("Saturday Mission Insert!");
 	});
 });
