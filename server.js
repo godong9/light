6440ad1156525b89cd2fd8f85383951bf3fdeede
godong9 <godong9@gmail.com>
2013-09-28 13:09:19 +0900

    130928_v2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var mysql_conn = require('./sql/mysql_server').mysql_conn;
 var schedule = require('node-schedule');
 
 
-//Sunday Job
+// Sunday Job
 var d0 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 0}, function(){
 	var email = 'allightdiet@gmail.com';
 	var group_id = 0;
@@ -20,7 +20,7 @@ var d0 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 0}, function(){
 	});
 });
 
-var h0 = schedule.scheduleJob({hour: 10, minute: 47, dayOfWeek: 6}, function(){
+var h0 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 0}, function(){
 	var sql = "SELECT ";
 		sql += "`email`, "; 
 		sql += "`day_calorie`, "; 
@@ -67,9 +67,11 @@ var h0 = schedule.scheduleJob({hour: 10, minute: 47, dayOfWeek: 6}, function(){
 		var u_query = mysql_conn.query(u_sql, function(err, rows, fields) {
 			console.log("Day 0 Reset!");
 		});
-
 	});
+});
 
+var c0 = schedule.scheduleJob({hour: 00, minute: 01, dayOfWeek: 0}, function(){
+	//기간 만료됬는지 확인하는 함수
 });
 
 var m0 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 0}, function(){
@@ -91,6 +93,7 @@ var m0 = schedule.scheduleJob({hour: 09, minute: 00, dayOfWeek: 0}, function(){
 	});
 });
 
+// Monday Job
 var d1 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 1}, function(){
     var email = 'allightdiet@gmail.com';
 	var group_id = 0;
@@ -105,11 +108,53 @@ var d1 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 1}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 1 Insert!");
+	});
+});
+
+var h1 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 1}, function(){
+	var sql = "SELECT ";
+		sql += "`email`, "; 
+		sql += "`day_calorie`, "; 
+		sql += "`food_calorie`, "; 
+		sql += "`exercise_calorie` ";
+		sql += "FROM `user` ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		// for 문 안에서 비동기 처리
+		for (var i=1; i<rows.length; i++)
+		{
+			(function (i) {
+				console.log(i);
+				var tmp_email = rows[i].email;
+				var tmp_day = rows[i].day_calorie;
+				var tmp_food = rows[i].food_calorie;
+				var tmp_exercise = rows[i].exercise_calorie;
+				var tmp_status_num = parseInt( Number(tmp_day) - (Number(tmp_food)-Number(tmp_exercise)) );
+				var tmp_status;
+				if(tmp_status_num > 0){
+					tmp_status = "good";
+				}
+				else{
+					tmp_status = "bad";
+				}
+				var i_sql = "INSERT INTO `user_history` ";
+				i_sql += "SET `email` = '"+tmp_email+"', ";
+				i_sql += "`status` = '"+tmp_status+"', ";
+				i_sql += "`food_calorie` = '"+tmp_food+"', ";
+				i_sql += "`exercise_calorie` = '"+tmp_exercise+"' ";
+
+				var i_query = mysql_conn.query(i_sql, function(err, rows, fields) {
+					console.log("tmp_email =>"+tmp_email);
+					console.log("tmp_status=>"+tmp_status);
+					console.log("tmp_food =>"+tmp_food);
+				});
+			}(i));
+		}
 
 		var u_sql = "UPDATE `user` ";
-			u_sql += "SET `food_calorie` = '0', "; 
-			u_sql += "`exercise_calorie` = '0' "; 
-
+		u_sql += "SET `food_calorie` = '0', "; 
+		u_sql += "`exercise_calorie` = '0' "; 
+		
 		var u_query = mysql_conn.query(u_sql, function(err, rows, fields) {
 			console.log("Day 1 Reset!");
 		});
@@ -149,11 +194,53 @@ var d2 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 2}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 2 Insert!");
+	});
+});
+
+var h2 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 2}, function(){
+	var sql = "SELECT ";
+		sql += "`email`, "; 
+		sql += "`day_calorie`, "; 
+		sql += "`food_calorie`, "; 
+		sql += "`exercise_calorie` ";
+		sql += "FROM `user` ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		// for 문 안에서 비동기 처리
+		for (var i=1; i<rows.length; i++)
+		{
+			(function (i) {
+				console.log(i);
+				var tmp_email = rows[i].email;
+				var tmp_day = rows[i].day_calorie;
+				var tmp_food = rows[i].food_calorie;
+				var tmp_exercise = rows[i].exercise_calorie;
+				var tmp_status_num = parseInt( Number(tmp_day) - (Number(tmp_food)-Number(tmp_exercise)) );
+				var tmp_status;
+				if(tmp_status_num > 0){
+					tmp_status = "good";
+				}
+				else{
+					tmp_status = "bad";
+				}
+				var i_sql = "INSERT INTO `user_history` ";
+				i_sql += "SET `email` = '"+tmp_email+"', ";
+				i_sql += "`status` = '"+tmp_status+"', ";
+				i_sql += "`food_calorie` = '"+tmp_food+"', ";
+				i_sql += "`exercise_calorie` = '"+tmp_exercise+"' ";
+
+				var i_query = mysql_conn.query(i_sql, function(err, rows, fields) {
+					console.log("tmp_email =>"+tmp_email);
+					console.log("tmp_status=>"+tmp_status);
+					console.log("tmp_food =>"+tmp_food);
+				});
+			}(i));
+		}
 
 		var u_sql = "UPDATE `user` ";
-			u_sql += "SET `food_calorie` = '0', "; 
-			u_sql += "`exercise_calorie` = '0' "; 
-
+		u_sql += "SET `food_calorie` = '0', "; 
+		u_sql += "`exercise_calorie` = '0' "; 
+		
 		var u_query = mysql_conn.query(u_sql, function(err, rows, fields) {
 			console.log("Day 2 Reset!");
 		});
@@ -193,11 +280,53 @@ var d3 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 3}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 3 Insert!");
+	});
+});
+
+var h3 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 3}, function(){
+	var sql = "SELECT ";
+		sql += "`email`, "; 
+		sql += "`day_calorie`, "; 
+		sql += "`food_calorie`, "; 
+		sql += "`exercise_calorie` ";
+		sql += "FROM `user` ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		// for 문 안에서 비동기 처리
+		for (var i=1; i<rows.length; i++)
+		{
+			(function (i) {
+				console.log(i);
+				var tmp_email = rows[i].email;
+				var tmp_day = rows[i].day_calorie;
+				var tmp_food = rows[i].food_calorie;
+				var tmp_exercise = rows[i].exercise_calorie;
+				var tmp_status_num = parseInt( Number(tmp_day) - (Number(tmp_food)-Number(tmp_exercise)) );
+				var tmp_status;
+				if(tmp_status_num > 0){
+					tmp_status = "good";
+				}
+				else{
+					tmp_status = "bad";
+				}
+				var i_sql = "INSERT INTO `user_history` ";
+				i_sql += "SET `email` = '"+tmp_email+"', ";
+				i_sql += "`status` = '"+tmp_status+"', ";
+				i_sql += "`food_calorie` = '"+tmp_food+"', ";
+				i_sql += "`exercise_calorie` = '"+tmp_exercise+"' ";
+
+				var i_query = mysql_conn.query(i_sql, function(err, rows, fields) {
+					console.log("tmp_email =>"+tmp_email);
+					console.log("tmp_status=>"+tmp_status);
+					console.log("tmp_food =>"+tmp_food);
+				});
+			}(i));
+		}
 
 		var u_sql = "UPDATE `user` ";
-			u_sql += "SET `food_calorie` = '0', "; 
-			u_sql += "`exercise_calorie` = '0' "; 
-
+		u_sql += "SET `food_calorie` = '0', "; 
+		u_sql += "`exercise_calorie` = '0' "; 
+		
 		var u_query = mysql_conn.query(u_sql, function(err, rows, fields) {
 			console.log("Day 3 Reset!");
 		});
@@ -237,11 +366,53 @@ var d4 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 4}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 4 Insert!");
+	});
+});
+
+var h4 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 4}, function(){
+	var sql = "SELECT ";
+		sql += "`email`, "; 
+		sql += "`day_calorie`, "; 
+		sql += "`food_calorie`, "; 
+		sql += "`exercise_calorie` ";
+		sql += "FROM `user` ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		// for 문 안에서 비동기 처리
+		for (var i=1; i<rows.length; i++)
+		{
+			(function (i) {
+				console.log(i);
+				var tmp_email = rows[i].email;
+				var tmp_day = rows[i].day_calorie;
+				var tmp_food = rows[i].food_calorie;
+				var tmp_exercise = rows[i].exercise_calorie;
+				var tmp_status_num = parseInt( Number(tmp_day) - (Number(tmp_food)-Number(tmp_exercise)) );
+				var tmp_status;
+				if(tmp_status_num > 0){
+					tmp_status = "good";
+				}
+				else{
+					tmp_status = "bad";
+				}
+				var i_sql = "INSERT INTO `user_history` ";
+				i_sql += "SET `email` = '"+tmp_email+"', ";
+				i_sql += "`status` = '"+tmp_status+"', ";
+				i_sql += "`food_calorie` = '"+tmp_food+"', ";
+				i_sql += "`exercise_calorie` = '"+tmp_exercise+"' ";
+
+				var i_query = mysql_conn.query(i_sql, function(err, rows, fields) {
+					console.log("tmp_email =>"+tmp_email);
+					console.log("tmp_status=>"+tmp_status);
+					console.log("tmp_food =>"+tmp_food);
+				});
+			}(i));
+		}
 
 		var u_sql = "UPDATE `user` ";
-			u_sql += "SET `food_calorie` = '0', "; 
-			u_sql += "`exercise_calorie` = '0' "; 
-
+		u_sql += "SET `food_calorie` = '0', "; 
+		u_sql += "`exercise_calorie` = '0' "; 
+		
 		var u_query = mysql_conn.query(u_sql, function(err, rows, fields) {
 			console.log("Day 4 Reset!");
 		});
@@ -281,11 +452,53 @@ var d5 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 5}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 5 Insert!");
+	});
+});
+
+var h5 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 5}, function(){
+	var sql = "SELECT ";
+		sql += "`email`, "; 
+		sql += "`day_calorie`, "; 
+		sql += "`food_calorie`, "; 
+		sql += "`exercise_calorie` ";
+		sql += "FROM `user` ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		// for 문 안에서 비동기 처리
+		for (var i=1; i<rows.length; i++)
+		{
+			(function (i) {
+				console.log(i);
+				var tmp_email = rows[i].email;
+				var tmp_day = rows[i].day_calorie;
+				var tmp_food = rows[i].food_calorie;
+				var tmp_exercise = rows[i].exercise_calorie;
+				var tmp_status_num = parseInt( Number(tmp_day) - (Number(tmp_food)-Number(tmp_exercise)) );
+				var tmp_status;
+				if(tmp_status_num > 0){
+					tmp_status = "good";
+				}
+				else{
+					tmp_status = "bad";
+				}
+				var i_sql = "INSERT INTO `user_history` ";
+				i_sql += "SET `email` = '"+tmp_email+"', ";
+				i_sql += "`status` = '"+tmp_status+"', ";
+				i_sql += "`food_calorie` = '"+tmp_food+"', ";
+				i_sql += "`exercise_calorie` = '"+tmp_exercise+"' ";
+
+				var i_query = mysql_conn.query(i_sql, function(err, rows, fields) {
+					console.log("tmp_email =>"+tmp_email);
+					console.log("tmp_status=>"+tmp_status);
+					console.log("tmp_food =>"+tmp_food);
+				});
+			}(i));
+		}
 
 		var u_sql = "UPDATE `user` ";
-			u_sql += "SET `food_calorie` = '0', "; 
-			u_sql += "`exercise_calorie` = '0' "; 
-
+		u_sql += "SET `food_calorie` = '0', "; 
+		u_sql += "`exercise_calorie` = '0' "; 
+		
 		var u_query = mysql_conn.query(u_sql, function(err, rows, fields) {
 			console.log("Day 5 Reset!");
 		});
@@ -325,11 +538,53 @@ var d6 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 6}, function(){
 
 	var query = mysql_conn.query(sql, function(err, rows, fields) {
 		console.log("Day 6 Insert!");
+	});
+});
+
+var h6 = schedule.scheduleJob({hour: 00, minute: 00, dayOfWeek: 6}, function(){
+	var sql = "SELECT ";
+		sql += "`email`, "; 
+		sql += "`day_calorie`, "; 
+		sql += "`food_calorie`, "; 
+		sql += "`exercise_calorie` ";
+		sql += "FROM `user` ";
+
+	var query = mysql_conn.query(sql, function(err, rows, fields) {
+		// for 문 안에서 비동기 처리
+		for (var i=1; i<rows.length; i++)
+		{
+			(function (i) {
+				console.log(i);
+				var tmp_email = rows[i].email;
+				var tmp_day = rows[i].day_calorie;
+				var tmp_food = rows[i].food_calorie;
+				var tmp_exercise = rows[i].exercise_calorie;
+				var tmp_status_num = parseInt( Number(tmp_day) - (Number(tmp_food)-Number(tmp_exercise)) );
+				var tmp_status;
+				if(tmp_status_num > 0){
+					tmp_status = "good";
+				}
+				else{
+					tmp_status = "bad";
+				}
+				var i_sql = "INSERT INTO `user_history` ";
+				i_sql += "SET `email` = '"+tmp_email+"', ";
+				i_sql += "`status` = '"+tmp_status+"', ";
+				i_sql += "`food_calorie` = '"+tmp_food+"', ";
+				i_sql += "`exercise_calorie` = '"+tmp_exercise+"' ";
+
+				var i_query = mysql_conn.query(i_sql, function(err, rows, fields) {
+					console.log("tmp_email =>"+tmp_email);
+					console.log("tmp_status=>"+tmp_status);
+					console.log("tmp_food =>"+tmp_food);
+				});
+			}(i));
+		}
 
 		var u_sql = "UPDATE `user` ";
-			u_sql += "SET `food_calorie` = '0', "; 
-			u_sql += "`exercise_calorie` = '0' "; 
-
+		u_sql += "SET `food_calorie` = '0', "; 
+		u_sql += "`exercise_calorie` = '0' "; 
+		
 		var u_query = mysql_conn.query(u_sql, function(err, rows, fields) {
 			console.log("Day 6 Reset!");
 		});
